6c353776f0f0daa0926f6e03cc3e3b069581290a
Matthew <matthew.b.rodriguez@gmail.com>
2019-11-15 16:54:56 -0500

    Reading works better now

--- server.js
+++ server.js
@@ -18,22 +18,36 @@ app.get('/express_backend', (req, res) => {
   res.send({ express: 'YOUR EXPRESS BACKEND IS CONNECTED TO REACT' });
 });
 
+// Firebase realtime database route GET route
 app.get('/database', (req, res) => {
-	var testRef = firebase.database().ref('test2/');
+    // TODO: FIGURE OUT HOW TO WAIT FOR testRef.on(); to finish running before
+    // running res.send(); ASYNC???
+
+
+    // Values to send back to React
+    var dataValues = new Object();
+    console.log("HELLO");
+
+    // Read Database
+	var testRef = firebase.database().ref('Users/');
 	testRef.on('value', function(snapshot) {
-		console.log("Found value");
         snapshot.forEach(function(snapshotRef) {
-            console.log("INNER");
-            console.log(snapshotRef.val());
+            var name = snapshotRef.val().Name;
+            var company = snapshotRef.val().Company;
+            dataValues[name] = company;
+            console.log(dataValues);
+            console.log("Name: ", name);
+            console.log("Company: ", company);
+            console.log("---------------");
         });
 	}, function (errorObject) {
       console.log("The read failed: " + errorObject.code);
     });
 
+    console.log("DATA VALUES: " + dataValues);
+
+    // Sending back JSON to React
     res.send({
-        "firebase-db": "CONNECTED TO FIREBASE DB"
+        "firebase-db-users": dataValues
     });
 });
-
-
-//var database = firebase.database();
