841455ee13853718b1d19a48e6eaf53be8d5ed52
Rageeb Mahtab <rmahtab0079@bths.edu>
2019-11-15 16:17:16 -0500

    Configuring firebase db

--- server.js
+++ server.js
@@ -1,6 +1,15 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 5000;
+var admin = require('firebase-admin');
+
+var serviceAccount = require("./firebase-credentials.json");
+
+var firebase = admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://connect-app-cebe4.firebaseio.com"
+});
+
 
 // console.log that your server is up and running
 app.listen(port, () => console.log(`Listening on port ${port}`));
@@ -9,3 +18,15 @@ app.listen(port, () => console.log(`Listening on port ${port}`));
 app.get('/express_backend', (req, res) => {
   res.send({ express: 'YOUR EXPRESS BACKEND IS CONNECTED TO REACT' });
 });
+
+app.get('/database', (req, res) => {
+	var testRef = firebase.database().ref('connect-app-cebe4/test');
+	testRef.on('value', function(snapshot) {
+		console.log("Found value");
+	});
+});
+
+
+//var database = firebase.database();
+
+
