f8f1c2ed7bc0aaef25418f78bbd90a876b8613b0
Matthew <matthew.b.rodriguez@gmail.com>
2019-11-15 16:33:08 -0500

    Found a way to read db

--- server.js
+++ server.js
@@ -1,15 +1,14 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 5000;
+
+// Initialize Firebase
 var admin = require('firebase-admin');
-
 var serviceAccount = require("./firebase-credentials.json");
-
 var firebase = admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
   databaseURL: "https://connect-app-cebe4.firebaseio.com"
 });
-
 
 // console.log that your server is up and running
 app.listen(port, () => console.log(`Listening on port ${port}`));
@@ -20,13 +19,21 @@ app.get('/express_backend', (req, res) => {
 });
 
 app.get('/database', (req, res) => {
-	var testRef = firebase.database().ref('connect-app-cebe4/test');
+	var testRef = firebase.database().ref('test2/');
 	testRef.on('value', function(snapshot) {
 		console.log("Found value");
-	});
+        snapshot.forEach(function(snapshotRef) {
+            console.log("INNER");
+            console.log(snapshotRef.val());
+        });
+	}, function (errorObject) {
+      console.log("The read failed: " + errorObject.code);
+    });
+
+    res.send({
+        "firebase-db": "CONNECTED TO FIREBASE DB"
+    });
 });
 
 
 //var database = firebase.database();
-
-
